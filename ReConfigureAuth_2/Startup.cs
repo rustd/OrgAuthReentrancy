78efecb336d9aa821950f1d203e80f4b722569d2
Pranav Rastogi <raspranav@gmail.com>
2015-01-15 17:53:57 -0800

    IndAuthOption2

---
+++ ReConfigureAuth_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReConfigureAuth_2.Startup))]
+namespace ReConfigureAuth_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
